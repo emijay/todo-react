74678daa92d22688ca35b4c9703bb47fc0a813f6
emijay <hilmi.johari@yahoo.com.sg>
2019-09-01 21:18:35 +0800

    able to delete items from list

--- script.jsx
+++ script.jsx
@@ -30,42 +30,60 @@ class List extends React.Component {
         let newList = this.state.list;
         newList.push(input)
         this.setState({list:newList});
+    }
 
+    handleInput = (event) => {
+
+        let arrayIndex = event.target.value;
+        // console.log(this.state.list[arrayIndex]);
+
+        let newList = this.state.list;
+        newList.splice(arrayIndex,1)
+        this.setState({list: newList})
     }
 
     render() {
 
-      console.log("rendering");
-      return (
-        <div className="container">
-            <h1 className="header mt-5">To Do List App</h1>
-            <div className="row form my-5">
-                <div className="my-5 pl-4 w-50">
-                    <Form receivingInput={(input) => {this.receivingInput(input)}} />
-                </div>
-            </div>
-            <div className="row list my-5">
-                <div className="col-8 mt-3 pl-4">
-                    <h2>List of Items To Do</h2>
-                </div>
-                <div className="col-4 mt-3 pr-5 text-right">
-                    <h3>Item Count:</h3>
-                </div>
-                <div className="pl-4">
-                    <ul>
-                        {this.state.list.map((item,i) => { return <li key={i} className="biggerFont">{item}</li>}) }
-                    </ul>
-                </div>
-            </div>
-            <div className="row deleted my-5">
-                <div className="my-5 pl-4">
-                    <h2>Deleted Items</h2>
-                    <ul>
+        console.log("rendering");
 
-                    </ul>
+        let item = this.state.list.map((item,i) => {
+            return(
+                <li key={i} className="biggerFont">{item}
+                    <button type="button" onClick={this.handleInput} className="btn btn-link" value={i}>Delete</button>
+                </li>
+            )
+        })
+
+        return (
+            <div className="container">
+                <h1 className="header mt-5">To Do List App</h1>
+                <div className="row form my-5">
+                    <div className="my-5 pl-4 w-50">
+                        <Form receivingInput={(input) => {this.receivingInput(input)}} />
+                    </div>
+                </div>
+                <div className="row list my-5">
+                    <div className="col-8 mt-3 pl-4">
+                        <h2>List of Items To Do</h2>
+                    </div>
+                    <div className="col-4 mt-3 pr-5 text-right">
+                        <h3>Item Count:</h3>
+                    </div>
+                    <div className="pl-4">
+                        <ul>
+                            {item}
+                        </ul>
+                    </div>
+                </div>
+                <div className="row deleted my-5">
+                    <div className="my-5 pl-4">
+                        <h2>Deleted Items</h2>
+                        <ul>
+
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
       );
     }
 }
